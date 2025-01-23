07a74c6f92753ce6d15bcf9d4f9178eeafced60e
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-23 05:06:01 +0100

    Update

--- views/announcement/index.blade.php
+++ views/announcement/index.blade.php
@@ -5,12 +5,12 @@
         @if ($announcements->count() > 0)
             <div class="grid grid-cols-12 gap-4">
                 @foreach ($announcements->sortByDesc('created_at') as $announcement)
-                    <div class="lg:col-span-4 md:col-span-6 col-span-12">
+                    <div class="col-span-12">
                         <div class="content-box">
-                            <h3 class="font-semibold text-lg">{{ $announcement->title }}</h3>
-                            <p>@markdownify(substr($announcement->announcement, 0, 100) . '...')</p>
+                            <h3 class="font-semibold text-secondary-200 text-lg">{{ $announcement->title }}</h3>
+                            <p class="text-secondary-200">@markdownify(substr($announcement->announcement, 0, 100) . '...')</p>
                             <div class="flex justify-between items-center mt-3">
-                                <span class="text-sm text-secondary-600">{{ __('Published') }}
+                                <span class="text-sm text-secondary-400">{{ __('Published') }}
                                     {{ $announcement->created_at->diffForHumans() }}</span>
                                 <a href="{{ route('announcements.view', $announcement->id) }}"
                                     class="button button-secondary">{{ __('Read More') }}</a>
