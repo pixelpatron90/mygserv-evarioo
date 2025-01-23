d6096e06c1b25026e48bbe72abd941db58800214
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-23 17:13:28 +0100

    Update

--- views/layouts/navigation.blade.php
+++ views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav class="lg:flex lg:flex-row lg:justify-between">
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-6 h-6 justify-center text-white rounded-sm lg:hidden bg-red-500 hover:bg-red-600" aria-controls="navbar-default" aria-expanded="false">
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-3.5 w-6 h-6 justify-center text-white rounded-sm lg:hidden bg-red-500 hover:bg-red-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <i class="fa-solid fa-bars"></i>
     </button>
