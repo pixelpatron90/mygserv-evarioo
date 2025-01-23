ac3db1f2c9a5dd07b8d8e0aa7a9a358b68699543
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-23 13:43:59 +0100

    Update

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -76,16 +76,27 @@
         <div class="container">
 
             <nav>
-                  <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                      <span class="sr-only">Open main menu</span>
-                      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                      </svg>
-                  </button>
-                  <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    fgdg
-                  </div>
-              </nav>
+                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                  fgdg
+                  <ul>
+                    <li><a href="{{ route('index') }}">{{ __('Home') }}</a></li>
+                    @foreach (App\Models\Category::whereNull('category_id')->orderBy('order')->get() as $category)
+                    @if ($category->products()->where('hidden', false)->count() > 0 || $category->children->count() > 0)
+                    <li><a href="{{ route('products', $category->slug) }}">{{ $category->name }}</a></li>
+                    @endif
+                    @endforeach
+                    <li><a href="{{ route('announcements.index') }}">{{ __('Announcements') }}</a></li>
+                    <li><a href="{{ route('media.index') }}">{{ __('Media') }}</a></li>
+                    <li><a href="{{ route('legalnotice.index') }}">{{ __('Legal notice') }}</a></li>
+                </ul>
+                </div>
+            </nav>
 
             <div class="site">
                 <div class="content">
