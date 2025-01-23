5de8b0a3f5330c80960b659bd17a00def99f1f99
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-23 16:56:37 +0100

    Update

--- views/livewire/cart-count.blade.php
+++ views/livewire/cart-count.blade.php
@@ -1,4 +1,4 @@
-<div class="h-full">
+<div class="h-full content-center flex items-center justify-center">
     @if ($cartItems > 0)
         <a href="{{ route('checkout.index') }}" class="button bg-red-500 hover:bg-red-600 text-white !font-normal">
             <i class="ri-shopping-bag-line"></i>
