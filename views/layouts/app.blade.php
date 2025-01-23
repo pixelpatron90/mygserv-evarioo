c49e0c78475c72a13da096cd1c872b72d25a8726
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-23 17:37:54 +0100

    Update

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -127,14 +127,153 @@
                             @endauth
                         </div>
                     </div>
+                    @empty(!$this->products)
                     <div class="widget">
                         <div class="title">
                             <h1>» Kundenmeinungen</h1>
                         </div>
                         <div class="content">
-                            np
+                            @if (empty($coupon))
+                        <div class="flex flex-row items-start gap-x-2 place-content-stretch">
+                            <div class="flex flex-col w-full">
+                                <x-input type="text" placeholder="{{ __('Coupon') }}" name="couponCode"
+                                    wire:model="couponCode" icon="ri-coupon-2-line" class="w-full" />
+                            </div>
+                            <button class="button button-primary" wire:click="validateCoupon">
+                                {{ __('Validate') }}
+                            </button>
+                        </div>
+                    @else
+                        <div class="flex items-center justify-between">
+                            <div>
+                                {{ __('Coupon:') }}
+                                <span class="text-secondary-900 font-semibold">{{ $coupon->code }}</span>
+                            </div>
+                            <button class="button button-danger" wire:click="removeCoupon">
+                                <i class="ri-delete-bin-2-line"></i>
+                            </button>
+                        </div>
+                    @endif
+                    <hr class="my-4 border-secondary-300">
+                    @foreach ($this->products as $product)
+                        @if ($product->price > 0)
+                            <span class=" -mb-3">
+                                {{ ucfirst($product->name) }}
+                            </span>
+                            <div class="flex flex-row items-center justify-between -mt-1 text-gray-500 text-sm">
+                                <div class="flex flex-row items-center">
+                                    <span>
+                                        {{ ucfirst($product->billing_cycle ?? 'One time') }}
+                                    </span>
+                                </div>
+                                <div class="flex flex-col">
+                                    <span>
+                                        @php
+                                            if ($product->quantity > 1) {
+                                                $quantity = $product->quantity . ' x';
+                                            } else {
+                                                $quantity = '';
+                                            }
+                                        @endphp
+                                        @if ($product->discount)
+                                            {{ $quantity }}
+                                            <x-money :amount="round($product->price - $product->discount, 2)" />
+                                        @else
+                                            {{ $quantity }}
+                                            <x-money :amount="$product->price" />
+                                        @endif
+                                    </span>
+                                </div>
+                            </div>
+                        @endif
+                        @if ($product->setup_fee > 0)
+                            <div class="flex flex-row items-center justify-between text-gray-500 text-sm">
+                                <div class="flex flex-row items-center">
+                                    {{ __('Setup fee') }}
+                                </div>
+                                <div class="flex flex-col">
+                                    {{ $quantity }}
+                                    <x-money :amount="$product->setup_fee - $product->discount_fee" />
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                    @if (!empty($discount))
+                        <div class="flex flex-row items-center justify-between mt-3">
+                            <div class="flex flex-row items-center">
+                                <span>{{ __('Discount') }}</span>
+                            </div>
+                            <div class="flex flex-col">
+                                <span>{{ config('settings::currency_sign') }}
+                                    {{ round($discount, 2) }}
+                                    @if ($coupon->type == 'percent')
+                                        ({{ $coupon->value }}%)
+                                    @endif
+                                </span>
+                            </div>
+                        </div>
+                    @endif
+                    <hr class="my-4 border-secondary-300">
+                    @if($tax->amount > 0)
+                        <div class="flex flex-row items-center justify-between mt-2">
+                            <div class="flex flex-row items-center">
+                                Subtotal
+                            </div>
+                            <div class="flex flex-col items-end">
+                                <x-money :amount="number_format($total-$tax->amount, 2)" />
+                            </div>
+                        </div>
+                        <div class="flex flex-row items-center justify-between mt-2">
+                            <div class="flex flex-row items-center">
+                                {{ $tax->name }} ({{ $tax->rate }}%)
+                            </div>
+                            <div class="flex flex-col items-end">
+                                <x-money :amount="$tax->amount" />
+                            </div>
+                        </div>
+                    @endif
+                    <div class="flex flex-row items-center justify-between mt-2">
+                        <div class="flex flex-row items-center">
+                            <span class="text-lg font-bold">{{ __('Total Today') }}</span>
+                        </div>
+                        <div class="flex flex-col items-end">
+                            <span class="text-lg font-bold">
+                                @if (!empty($discount))
+                                    <x-money :amount="$total - $discount" />
+                                @else
+                                    <x-money :amount="$total" />
+                                @endif
+                            </span>
                         </div>
                     </div>
+
+                    <hr class="my-4 border-secondary-300">
+                    <form wire:submit.prevent="pay">
+                        {{ __('You can continue without paying') }}
+                        @if (config('settings::tos'))
+                            <div class="items-center p-1">
+                                @php
+                                $tos = "I agree to the <a href='" . route('tos') . "' class='text-blue-500 hover:text-blue-600'>terms of service</a>"; @endphp <x-input id="tos" type="checkbox" name="tos" required
+                                    :label="$tos" wire:model="tos" />
+                            </div>
+                        @endif
+                        <div class="flex justify-end mt-4">
+                            <button class="button button-primary" type="submit">
+                                <span wire:loading wire:target="pay">
+                                    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="#9095A0" stroke-width="4"></circle>
+                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                    </svg>
+                                </span>
+                                <span wire:loading.remove wire:target="pay">
+                                    {{ __('Checkout') }}
+                                </span>
+                            </button>
+                        </div>
+                    </form>
+                        </div>
+                    </div>
+                    @endempty
                 </div>
             </div>
         </div>
