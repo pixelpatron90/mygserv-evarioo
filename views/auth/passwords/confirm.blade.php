57682667d034c37dbab4eee3bd30f4e5781650d5
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-23 15:26:38 +0100

    Update

--- views/auth/passwords/confirm.blade.php
+++ views/auth/passwords/confirm.blade.php
@@ -1,16 +1,14 @@
 <x-app-layout>
     <div class="min-h-[50vh] flex items-center justify-center flex-col">
-        <div class="w-full px-6 py-4 mt-6 overflow-hidden rounded-sm content-box">
+        <div class="w-full content-box">
             <div class="mb-4 text-red-500">
                 {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
             </div>
 
             <form method="POST" action="{{ route('password.confirm') }}" id="pw-confirm">
                 @csrf
-
                 <!-- Password -->
-                <div class="mt-4">
-
+                <div class="mt-4 mb-4">
                     <x-input id="password" type="password" label="{{ __('Password') }}"
                         name="password" required autocomplete="new-password" >
                     </x-input>
@@ -18,7 +16,7 @@
                 <x-recaptcha form="pw-confirm" />
                 <div class="flex justify-end mt-4">
                     <button type="submit"
-                        class="button button-primary">
+                        class="button bg-red-500 hover:text-red-600 text-white">
                         {{ __('Confirm') }}
                     </button>
                 </div>
