44d1afe82b9716c2f28f9e833aac73b59e9213b1
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 01:20:40 +0100

    Update

--- views/layouts/admin.blade.php
+++ views/layouts/admin.blade.php
@@ -115,7 +115,7 @@
                 <div>
                     <p>Copyright © 2025 - 2026 www.mygserv.de & <a class="text-red-500 hover:text-red-600" href="https://www.evarioo.eu">www.evarioo.eu</a> | Alle Rechte vorbehalten.</p>
                 </div>
-                <div>
+                <div class="lg:block hidden">
                     <ul>
                         <li>
                             <a class="text-red-500 hover:text-red-600" href="https://www.evarioo.eu">Startseite</a>
