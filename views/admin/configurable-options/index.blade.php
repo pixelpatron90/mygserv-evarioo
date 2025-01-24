ee98dcfb37e74a4a7ac4d226fa73dab48789fae2
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 22:42:10 +0100

    Uodate

--- views/admin/configurable-options/index.blade.php
+++ views/admin/configurable-options/index.blade.php
@@ -1,11 +1,17 @@
 <x-admin-layout title="Configurable Options">
-    <h1 class="text-2xl font-bold dark:text-darkmodetext">{{ __('Configurable Options') }}</h1>
-    <div class="flex justify-end pr-3 pt-3">
-        <a href="{{ route('admin.configurable-options.create') }}">
-            <button class="button button-primary">
-                {{ __('Create') }}
-            </button>
-        </a>
+    <div class="p-3 site-header">
+        <div>
+            <div class="text-secondary-200 text-2xl font-bold">
+                <i class="fa-solid fa-caret-right"></i> {{ __('Configurable Options') }}
+            </div>
+        </div>
+        <div class="flex my-auto float-end justify-end mr-4">
+            <a href="{{ route('admin.configurable-options.create') }}"
+               class="px-4 py-2 font-bold !text-white transition rounded delay-400 button button-primary">
+               <i class="fa-solid !text-white fa-plus me-2"></i> {{ __('Create') }}
+            </a>
+        </div>
     </div>
+
     <livewire:admin.configurable-options />
 </x-admin-layout>
