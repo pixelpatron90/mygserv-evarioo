d107bf8d2356ff51212f24fbbce24b95459865c5
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 22:37:42 +0100

    Uodate

--- views/admin/pages/index.blade.php
+++ views/admin/pages/index.blade.php
@@ -2,14 +2,14 @@
     <div class="p-3 site-header">
         <div>
             <div class="text-secondary-200 text-2xl font-bold">
-                <i class="fa-solid fa-caret-right"></i> {{ __('Products') }}
+                <i class="fa-solid fa-caret-right"></i> {{ __('Pages') }}
             </div>
             <p class="text-secondary-200">
-                {{ __('Here you can see all products.') }}
+                {{ __('Here you can see all pages.') }}
             </p>
         </div>
         <div class="flex my-auto float-end justify-end mr-4">
-            <a href="{{ route('admin.products.create') }}"
+            <a href="{{ route('admin.page.create') }}"
                class="px-4 py-2 font-bold !text-white transition rounded delay-400 button button-primary">
                <i class="fa-solid !text-white fa-plus me-2"></i> {{ __('Create') }}
             </a>
