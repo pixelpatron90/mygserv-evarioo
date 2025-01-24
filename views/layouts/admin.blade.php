5fc9f444d7b2a1e73338da403c21464f6a9cddb8
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 01:59:16 +0100

    Update

--- views/layouts/admin.blade.php
+++ views/layouts/admin.blade.php
@@ -90,21 +90,15 @@
                         <a id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="cursor-pointer">
                             Dropdown
                         </a>
-                        <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                            <ul class="flex lg:flex-col flex-row" aria-labelledby="dropdownLargeButton">
+                        <div id="dropdownNavbar" class="z-10 hidden bg-white rounded-md shadow lg:w-44 w-full">
+                            <ul class="" aria-labelledby="dropdownLargeButton">
                               <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                <a href="#" class="block w-full">Dashboard</a>
                               </li>
                               <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                              </li>
-                              <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                                <a href="#" class="block w-full">Settings</a>
                               </li>
                             </ul>
-                            <div class="py-1">
-                              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-                            </div>
                         </div>
                     </li>
                 </ul>
