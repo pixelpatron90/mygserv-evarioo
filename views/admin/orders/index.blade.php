d19be63dfc052f1d424981bb121aa70bfd8de353
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 21:06:27 +0100

    Update

--- views/admin/orders/index.blade.php
+++ views/admin/orders/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">
         {{ __('Orders') }}
     </x-slot>
-    <h1 class="text-center text-2xl font-bold">{{ __('Orders') }}</h1>
+    <h1 class="text-center text-secondary-200 text-2xl font-bold">{{ __('Orders') }}</h1>
 
     <livewire:admin.orders />
 </x-admin-layout>
--- views/admin/products/index.blade.php
+++ views/admin/products/index.blade.php
@@ -11,7 +11,7 @@
         <div class="flex my-auto float-end justify-end mr-4">
             <a href="{{ route('admin.products.create') }}"
                class="px-4 py-2 font-bold text-white transition rounded delay-400 bg-blue-500 button button-primary">
-                <i class="ri-user-add-line"></i> {{ __('Create') }}
+               <i class="fa-solid fa-plus me-2"></i> {{ __('Create') }}
             </a>
         </div>
     </div>
