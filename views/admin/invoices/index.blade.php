e720352a2d814b92dc1518d5d0ece85c25da4b09
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 21:08:20 +0100

    Update

--- views/admin/invoices/index.blade.php
+++ views/admin/invoices/index.blade.php
@@ -2,14 +2,14 @@
     <x-slot name="title">
         {{ __('Invoices') }}
     </x-slot>
-    <div class="p-3 bg-white dark:bg-secondary-100 flex flex-row justify-between">
+    <div class="p-3 site-header">
         <div>
-            <div class="mt-3 text-2xl font-bold dark:text-darkmodetext">
+            <div class="text-2xl font-bold text-secondary-200">
                 {{ __('Invoices') }}
             </div>
-            <div class="mt-3 text-gray-500 dark:text-darkmodetext">
+            <p class="text-secondary-200">
                 {{ __('Here you can see all invoices.') }}
-            </div>
+            </p>
         </div>
         <div class="flex my-auto float-end justify-end mr-4">
             <a href="{{ route('admin.invoices.create') }}" class="px-4 py-2 font-bold text-white transition rounded delay-400 button button-primary">
