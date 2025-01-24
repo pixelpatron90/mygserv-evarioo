b62ee04115ea1cae2e182b29356354984a6911c2
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-24 22:34:03 +0100

    Uodate

---
+++ views/admin/pages/index.blade.php
@@ -0,0 +1,21 @@
+<x-admin-layout title="Products">
+    <div class="p-3 site-header">
+        <div>
+            <div class="text-secondary-200 text-2xl font-bold">
+                <i class="fa-solid fa-caret-right"></i> {{ __('Products') }}
+            </div>
+            <p class="text-secondary-200">
+                {{ __('Here you can see all products.') }}
+            </p>
+        </div>
+        <div class="flex my-auto float-end justify-end mr-4">
+            <a href="{{ route('admin.products.create') }}"
+               class="px-4 py-2 font-bold !text-white transition rounded delay-400 button button-primary">
+               <i class="fa-solid !text-white fa-plus me-2"></i> {{ __('Create') }}
+            </a>
+        </div>
+    </div>
+    
+    Kommt bald
+
+</x-admin-layout>
