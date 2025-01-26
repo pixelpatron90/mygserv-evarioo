3d922401b36cb0fb7be2ed9f025ff9eb33f46f7d
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-26 15:07:18 +0100

    Update

--- views/auth/register.blade.php
+++ views/auth/register.blade.php
@@ -14,8 +14,8 @@
 
             <form method="POST" action="{{ route('register') }}" id="register">
                 @csrf
-                <div class="flex col-span-6 flex-row gap-4 mb-4">
-                    <div class="w-3/6">
+                <div class="flex flex-col-3 flex-row gap-4 mb-4">
+                    <div class=" w-[50%]">
                         <label class="form-label" for="first_name">{{ __('First name') }}</label>
                         <input type="name" class="form-input" placeholder="{{ __('First name') }}" name="first_name" id="first_name" />
                     </div>
