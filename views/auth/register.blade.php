237fd97523346f65e662cdc310a10e9313024785
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-26 15:11:49 +0100

    Update

--- views/auth/register.blade.php
+++ views/auth/register.blade.php
@@ -14,16 +14,16 @@
 
             <form method="POST" action="{{ route('register') }}" id="register">
                 @csrf
-                <div class="flex flex-row gap-4 mb-4">
-                    <div class="flex-1">
+                <div class="flex flex-wrap gap-4 mb-4">
+                    <div class="w-3/6">
                         <label class="form-label" for="first_name">{{ __('First name') }}</label>
                         <input type="name" class="form-input" placeholder="{{ __('First name') }}" name="first_name" id="first_name" />
                     </div>
-                    <div class="flex-1">
+                    <div class="w-3/6">
                         <label class="form-label" for="last_name">{{ __('Last name') }}</label>
                         <input type="name" class="form-input" placeholder="{{ __('Last name') }}" name="last_name" id="last_name" />
                     </div>
-                    <div class="flex-1">
+                    <div class="w-full">
                         <label class="form-label" for="username">{{ __('Username') }}</label>
                         <input type="name" class="form-input" placeholder="{{ __('Username') }}" name="username" id="username" />
                     </div>
@@ -32,7 +32,7 @@
                 <label class="form-label" for="email">{{ __('Email') }}</label>
                 <input type="email" class="form-input" placeholder="{{ __('Email..') }}" name="email" id="email" />
 
-                <div class="flex flex-row gap-4 mt-4">
+                <div class="flex flex-wrap gap-4 mt-4">
                     <div class="w-3/6">
                         <label class="form-label" for="last_name">{{ __('Password') }}</label>
                         <input type="password" required class="form-input" placeholder="{{ __('Password') }}" name="password" id="password" />
