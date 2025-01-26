fc269c6350b5a057806eb8911b8cf8ab653e6b37
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-26 21:50:36 +0100

    Update

--- views/auth/passwords/forget.blade.php
+++ views/auth/passwords/forget.blade.php
@@ -1,28 +1,23 @@
 <x-app-layout>
 
-    <div class="content min-h-[50vh] flex items-center justify-center flex-col">
-        <div class="flex items-center text-secondary-900 font-semibold text-lg py-4 gap-x-2">
-            <x-application-logo class="w-10" />
-            {{ config('app.name', 'Paymenter') }}
-        </div>
-        <div class="content-box max-w-lg w-full">
+    <div class="flex items-center justify-center flex-col">
+        <div class="content-box">
+            <h2 class="text-lg text-secondary-200 font-semibold border-b-2 border-red-500 pb-2 mb-4">{{ __('Forgot Password') }}</h2>
+            <p>
+                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            </p>
             <form method="POST" action="{{ route('password.email') }}" id="forget-password">
                 @csrf
-                
-                <h2 class="text-lg font-semibold">{{ __('Forgot Password') }}</h2>
-                <p>
-                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-                </p>
-                
-                <x-input class="mt-3" label="{{ __('Email') }}" type="email" placeholder="{{ __('Email..') }}" required
-                    name="email" id="email" icon="ri-at-line" />
 
-                <x-recaptcha form="forget-password" />
+                <label class="form-label" for="email">{{ __('Email') }}</label>
+                <input type="email" class="form-input" placeholder="{{ __('Email..') }}" name="email" id="email" />
+
+                <x-recaptcha class="mt-2" form="forget-password" />
                 <div class="mt-3 flex justify-between items-center">
-                    <a href="{{ route('login') }}" class="text-sm text-secondary-600 underline">
+                    <a href="{{ route('login') }}" class="text-sm text-red-500 hover:text-red-600">
                         {{ __('Return to Login') }}
                     </a>
-                    <button type="submit" class="button button-primary">
+                    <button type="submit" class="form-submit">
                         {{ __('Email Password Reset Link') }}
                     </button>
                 </div>
