4c72e5b2df48675cfe2aadd5886e792ab5eba52c
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 16:06:59 +0100

    Uodate

--- views/admin/products/edit.blade.php
+++ views/admin/products/edit.blade.php
@@ -3,7 +3,7 @@
         {{ __('Products') }}
     </x-slot>
     @include('admin.products.tabbar')
-    <div class="grid grid-cols-1 md:grid-cols-2 mt-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 mt-4 mb-4">
         <div class="text-2xl dark:text-darkmodetext">
             {{ __('Update product') }} {{ $product->name }}
         </div>
@@ -40,85 +40,83 @@
             </form>
         </div>
     </div>
-    <div class="mt-6 text-gray-500 dark:text-darkmodetext dark:bg-secondary-100">
-        <form method="POST" action="{{ route('admin.products.update', $product->id) }}" enctype="multipart/form-data">
-            @csrf
-            <x-input type="text" name="name" label="{{ __('Name') }}" placeholder="{{ __('Name') }}"
-                value="{{ $product->name }}" required autofocus />
+    <form method="POST" action="{{ route('admin.products.update', $product->id) }}" enctype="multipart/form-data">
+        @csrf
+        <x-input type="text" name="name" label="{{ __('Name') }}" placeholder="{{ __('Name') }}"
+            value="{{ $product->name }}" required autofocus />
 
-            <x-input type="checkbox" label="{{ __('Hidden') }}" name="hidden" id="hidden" value="1" class="mt-2"
-                :checked="$product->hidden ? true : false" />
-                
-            <x-input type="textarea" name="description" label="{{ __('Description') }}"
-                placeholder="{{ __('Description') }}" value="{{ $product->description }}" required rows="4" />
+        <x-input type="checkbox" label="{{ __('Hidden') }}" name="hidden" id="hidden" value="1" class="mt-2"
+            :checked="$product->hidden ? true : false" />
+            
+        <x-input type="textarea" name="description" label="{{ __('Description') }}"
+            placeholder="{{ __('Description') }}" value="{{ $product->description }}" required rows="4" />
 
 
-            <x-input type="checkbox" label="{{ __('Stock enabled') }}" name="stock_enabled" id="stock_enabled"
-                value="1"
-                onchange="if(this.checked) { document.getElementById('stock').classList.remove('hidden'); } else { document.getElementById('stock').classList.add('hidden'); }"
-                :checked="$product->stock_enabled ? true : false" />
+        <x-input type="checkbox" label="{{ __('Stock enabled') }}" name="stock_enabled" id="stock_enabled"
+            value="1"
+            onchange="if(this.checked) { document.getElementById('stock').classList.remove('hidden'); } else { document.getElementById('stock').classList.add('hidden'); }"
+            :checked="$product->stock_enabled ? true : false" />
 
-            <div class="@if (!$product->stock_enabled) hidden @endif" id="stock">
-                <x-input type="number" name="stock" label="{{ __('Stock') }}" placeholder="{{ __('Stock') }}"
-                    value="{{ $product->stock }}" required min="0" />
-            </div>
+        <div class="@if (!$product->stock_enabled) hidden @endif" id="stock">
+            <x-input type="number" name="stock" label="{{ __('Stock') }}" placeholder="{{ __('Stock') }}"
+                value="{{ $product->stock }}" required min="0" />
+        </div>
 
-            <div class="mt-4">
-                <label for="image">{{ __('Image') }}</label>
-                <p>Only upload a new image if you want to replace the existing one</p>
-                <input id="image" class="block w-full mt-1 rounded-lg dark:bg-darkmode" type="file"
-                    name="image" @if ($product->image == 'null') disabled @endif />
-                <div class="mt-2">
-                    <label for="no_image">No Image</label>
-                    <input type="checkbox" name="no_image" id="no_image" value="1" class="form-input w-4 h-4"
-                        @if ($product->image == 'null') checked @endif>
-                    <img src="{{ $product->image }}" alt="{{ $product->name }}" class="w-32 h-32 mt-4" id="prodctimg"
-                        onerror="removeElement(this)">
-                    <script>
-                        function removeElement(element) {
-                            element.onerror = "";
+        <div class="mt-4">
+            <label for="image">{{ __('Image') }}</label>
+            <p>Only upload a new image if you want to replace the existing one</p>
+            <input id="image" class="block w-full mt-1 rounded-lg dark:bg-darkmode" type="file"
+                name="image" @if ($product->image == 'null') disabled @endif />
+            <div class="mt-2">
+                <label for="no_image">No Image</label>
+                <input type="checkbox" name="no_image" id="no_image" value="1" class="form-input w-4 h-4"
+                    @if ($product->image == 'null') checked @endif>
+                <img src="{{ $product->image }}" alt="{{ $product->name }}" class="w-32 h-32 mt-4" id="prodctimg"
+                    onerror="removeElement(this)">
+                <script>
+                    function removeElement(element) {
+                        element.onerror = "";
+                    }
+                    document.getElementById('no_image').addEventListener('change', function() {
+                        document.getElementById('image').disabled = this.checked;
+                        document.getElementById('prodctimg').classList.toggle('hidden');
+                    });
+                    // Listen for file uploads then display the image
+                    document.getElementById('image').addEventListener('change', function() {
+                        if (this.files && this.files[0]) {
+                            var img = document.getElementById('prodctimg');
+                            img.classList.remove('hidden');
+                            img.src = URL.createObjectURL(this.files[0]);
                         }
-                        document.getElementById('no_image').addEventListener('change', function() {
-                            document.getElementById('image').disabled = this.checked;
-                            document.getElementById('prodctimg').classList.toggle('hidden');
-                        });
-                        // Listen for file uploads then display the image
-                        document.getElementById('image').addEventListener('change', function() {
-                            if (this.files && this.files[0]) {
-                                var img = document.getElementById('prodctimg');
-                                img.classList.remove('hidden');
-                                img.src = URL.createObjectURL(this.files[0]);
-                            }
-                        });
-                        if (document.getElementById('no_image').checked) {
-                            document.getElementById('image').disabled = true;
-                            document.getElementById('prodctimg').classList.add('hidden');
-                        }
-                    </script>
-                </div>
+                    });
+                    if (document.getElementById('no_image').checked) {
+                        document.getElementById('image').disabled = true;
+                        document.getElementById('prodctimg').classList.add('hidden');
+                    }
+                </script>
             </div>
-            <x-input type="select" name="category_id" label="{{ __('Category') }}">
-                @if ($categories->count())
-                    @foreach ($categories as $category)
-                        @if ($category->id == $product->category_id)
-                            <option value="{{ $category->id }}" selected>{{ $category->name }}
-                            </option>
-                        @else
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endif
-                    @endforeach
-                @else
-                    <option value="">No categories found</option>
-                @endif
-            </x-input>
-            <div class="flex items-center justify-end mt-4 text-blue-700">
-                <a href="{{ route('admin.categories.create') }}">Create Category</a>
-            </div>
-            <div class="flex items-center justify-end mt-4">
-                <button type="submit" class="inline-flex justify-center w-max float-right button button-primary">
-                    {{ __('Save') }}
-                </button>
-            </div>
-        </form>
-    </div>
+        </div>
+        <x-input type="select" name="category_id" label="{{ __('Category') }}">
+            @if ($categories->count())
+                @foreach ($categories as $category)
+                    @if ($category->id == $product->category_id)
+                        <option value="{{ $category->id }}" selected>{{ $category->name }}
+                        </option>
+                    @else
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endif
+                @endforeach
+            @else
+                <option value="">No categories found</option>
+            @endif
+        </x-input>
+        <div class="flex items-center justify-end mt-4 text-blue-700">
+            <a href="{{ route('admin.categories.create') }}">Create Category</a>
+        </div>
+        <div class="flex items-center justify-end mt-4">
+            <button type="submit" class="inline-flex justify-center w-max float-right button button-primary">
+                {{ __('Save') }}
+            </button>
+        </div>
+    </form>
 </x-admin-layout>
