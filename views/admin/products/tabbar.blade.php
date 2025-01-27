78ad28124af625a624066e9de02b69922e449922
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 15:52:49 +0100

    Uodate

--- views/admin/products/tabbar.blade.php
+++ views/admin/products/tabbar.blade.php
@@ -2,26 +2,26 @@
     <ul class="flex flex-row">
         <li>
             <a href="{{ route('admin.products.edit', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold uppercase border-b-2 dark:text-darkmodetext dark:hover:bg-darkbutton border-logo text-logo">
+                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold uppercase border-b-2 border-logo text-logo">
                 {{ __('Details') }}
             </a>
         </li>
 
         <li>
             <a href="{{ route('admin.products.pricing', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold text-gray-900 uppercase border-b-2 dark:text-darkmodetext dark:hover:bg-darkbutton border-y-transparent hover:border-logo hover:text-logo">
+                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold text-gray-900 uppercase border-b-2 border-y-transparent hover:border-red-500 hover:text-red-500">
                 {{ __('Pricing') }}
             </a>
         </li>
         <li>
             <a href="{{ route('admin.products.extension', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold text-gray-900 uppercase border-b-2 dark:text-darkmodetext dark:hover:bg-darkbutton border-y-transparent hover:border-logo hover:text-logo">
+                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold text-gray-900 uppercase border-b-2 border-y-transparent hover:border-red-500 hover:text-red-500">
                 {{ __('Extension') }}
             </a>
         </li>
         <li>
             <a href="{{ route('admin.products.upgrade', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold text-gray-900 uppercase border-b-2 dark:text-darkmodetext dark:hover:bg-darkbutton border-y-transparent hover:border-logo hover:text-logo">
+                class="inline-flex justify-center w-full p-4 px-2 py-2 text-xs font-bold text-gray-900 uppercase border-b-2 border-y-transparent hover:border-red-500 hover:text-red-500">
                 {{ __('Upgrades') }}
             </a>
         </li>
