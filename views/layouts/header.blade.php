b3fd7846d3ad0517f7064f31593e00457cea5d63
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 13:57:18 +0100

    Update

--- views/layouts/header.blade.php
+++ views/layouts/header.blade.php
@@ -5,14 +5,15 @@
                 <a href="{{ route('index') }}">
                     <x-branding class="lg:w-36 w-20" />
                 </a>
-                <div class="title">
-                    <h1>
-                        <a class="hover:text-red-600" href="{{ route('index') }}">
-                            {{ config('app.name') }}
-                        </a>
-                    </h1>
-                    <p>{{ config('app.slogan', 'Machs einfach. Zock kostenlos!') }}</p>
-                </div>
+                
+            </div>
+            <div class="login">
+                <h1>
+                    <a class="hover:text-red-600" href="{{ route('index') }}">
+                        {{ config('app.name') }}
+                    </a>
+                </h1>
+                <p>{{ config('app.slogan', 'Machs einfach. Zock kostenlos!') }}</p>
             </div>
         </div>
     </div>
