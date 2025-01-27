aecc7e38761debf87d60579568e4b1bd4bcbdbc6
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 12:37:24 +0100

    Update

--- views/layouts/topbar.blade.php
+++ views/layouts/topbar.blade.php
@@ -5,7 +5,7 @@
                 {{ __('Welcome to :appname', ['appname' => config('app.name')]) }}
             </p>
             <div>
-                <img src="./images/icons/flags/de.png" />
+                <img src="./themes/mygserv-evarioo/images/icons/flags/de.png" />
             </div>
         </div>
     </div>
