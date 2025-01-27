d7203c0cb9ab134b096a0a01cc38cd165068dc5f
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 16:00:48 +0100

    Uodate

--- views/admin/products/tabbar.blade.php
+++ views/admin/products/tabbar.blade.php
@@ -2,26 +2,26 @@
     <ul class="flex flex-row text-white text-sm border-b-4 border-red-500">
         <li>
             <a href="{{ route('admin.products.edit', $product->id) }}"
-                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full px-2.5 py-3.5 font-bold uppercase">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white rounded-t-md w-full px-2.5 py-3.5 font-bold uppercase">
                 {{ __('Details') }}
             </a>
         </li>
 
         <li>
             <a href="{{ route('admin.products.pricing', $product->id) }}"
-                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full px-2 py-2 font-bold uppercase">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white rounded-t-md w-full px-2.5 py-3.5 font-bold uppercase">
                 {{ __('Pricing') }}
             </a>
         </li>
         <li>
             <a href="{{ route('admin.products.extension', $product->id) }}"
-                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full px-2 py-2 font-bold uppercase">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white rounded-t-md w-full px-2.5 py-3.5 font-bold uppercase">
                 {{ __('Extension') }}
             </a>
         </li>
         <li>
             <a href="{{ route('admin.products.upgrade', $product->id) }}"
-                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full px-2 py-2 font-bold uppercase">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white rounded-t-md w-full px-2.5 py-3.5 font-bold uppercase">
                 {{ __('Upgrades') }}
             </a>
         </li>
