3b9e253bdffa98e7b04446e7ff7c445c62cbf85c
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 15:57:48 +0100

    Uodate

--- views/admin/products/tabbar.blade.php
+++ views/admin/products/tabbar.blade.php
@@ -1,27 +1,27 @@
 <div class="mb-4">
-    <ul class="flex flex-row text-white text-sm">
+    <ul class="flex flex-row text-white text-sm border-b-4 border-red-500">
         <li>
             <a href="{{ route('admin.products.edit', $product->id) }}"
-                class="inline-flex justify-center hover:bg-red-500 w-full p-4 px-2 py-2 font-bold uppercase border-b-2 hover:border-red-500 hover:text-red-500">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full p-4 px-2 py-2 font-bold uppercase">
                 {{ __('Details') }}
             </a>
         </li>
 
         <li>
             <a href="{{ route('admin.products.pricing', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 font-bold uppercase border-b-2 border-y-transparent hover:border-red-500 hover:text-red-500">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full p-4 px-2 py-2 font-bold uppercase">
                 {{ __('Pricing') }}
             </a>
         </li>
         <li>
             <a href="{{ route('admin.products.extension', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 font-bold uppercase border-b-2 border-y-transparent hover:border-red-500 hover:text-red-500">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full p-4 px-2 py-2 font-bold uppercase">
                 {{ __('Extension') }}
             </a>
         </li>
         <li>
             <a href="{{ route('admin.products.upgrade', $product->id) }}"
-                class="inline-flex justify-center w-full p-4 px-2 py-2 font-bold uppercase border-b-2 border-y-transparent hover:border-red-500 hover:text-red-500">
+                class="inline-flex justify-center hover:bg-red-500 hover:text-white w-full p-4 px-2 py-2 font-bold uppercase">
                 {{ __('Upgrades') }}
             </a>
         </li>
