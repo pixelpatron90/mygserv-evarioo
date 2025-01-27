663c4c41b9300f20612e38f17834125f9ecd4df4
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 18:06:23 +0100

    Update

--- views/admin/products/upgrade.blade.php
+++ views/admin/products/upgrade.blade.php
@@ -5,25 +5,23 @@
     </div>
     <form action="{{ route('admin.products.upgrade.update', $product->id) }}" method="POST">
         @csrf
-        <div>
+        <div class="mb-4">
             <x-input type="select" multiple name="upgrades[]" class="mt-2">
                 @foreach ($products as $product2)
                 <option value="{{ $product2->id }}" @if ($product->upgrades()->where('upgrade_product_id',
                     $product2->id)->exists()) selected @endif>{{ $product2->name }}</option>
                 @endforeach
             </x-input>
-
-            <div class="hidden tobereleased">
-                <x-input class="mt-2" type="checkbox" value="1" name="upgrade_configurable_options"
-                    label="{{ __('Allow upgrade of configurable options') }}"
-                    :checked="$product->upgrade_configurable_options ? true : false" />
-            </div>
-
-            <div class="flex items-center justify-start">
-                <button type="submit" class="form-submit">
-                    {{ __('Update') }}
-                </button>
-            </div>
+        </div>
+        <div class="hidden mb-4 tobereleased">
+            <x-input class="mt-2" type="checkbox" value="1" name="upgrade_configurable_options"
+                label="{{ __('Allow upgrade of configurable options') }}"
+                :checked="$product->upgrade_configurable_options ? true : false" />
+        </div>
+        <div class="flex items-center justify-start">
+            <button type="submit" class="form-submit">
+                {{ __('Update') }}
+            </button>
         </div>
     </form>
 </x-admin-layout>
