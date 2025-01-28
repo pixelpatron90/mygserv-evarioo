f403f7f7840cb852c2d1a3083e72160f46e3f9e4
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 12:00:36 +0100

    Update

--- views/layouts/maintenance.blade.php
+++ views/layouts/maintenance.blade.php
@@ -13,14 +13,12 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
     @endif
-
     <meta content="{{ config('settings::seo_title') }}" property="og:title" />
     <meta content="{{ config('settings::seo_description') }}" property="og:description" />
     <meta content="{{ config('settings::seo_description') }}" name="description" />
     <meta content='{{ config(' settings::seo_image') }}' property='og:image'>
     <meta name="twitter:card" content="@if (config('settings::seo_twitter_card')) summary_large_image @endif" />
-
-    <title>{{ config('app.name') }} | {{ __('Authentication') }}</title>
+    <title>{{ config('app.name') }} | {{ __('Maintenance') }}</title>
 </head>
 
 <body class="auth">
@@ -32,7 +30,11 @@
         <div class="hidden lg:flex justify-center items-center py-4">
             <x-branding class="w-36" />
         </div>
-        dsdfddsf
+        <div class="bg-gray-200 p-2">
+            <h1>
+                {{ __('Maintenance on working') }}
+            </h1>
+        </div>
     </div>
 
 </body>
