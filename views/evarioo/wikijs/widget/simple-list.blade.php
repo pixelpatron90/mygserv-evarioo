b75cb42f8321a3357ae37e1e5e986767610c1fb5
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 15:54:15 +0100

    Update

--- views/evarioo/wikijs/widget/simple-list.blade.php
+++ views/evarioo/wikijs/widget/simple-list.blade.php
@@ -8,9 +8,9 @@
     <div class="content">
         <ul>
             @foreach ($data->pages as $page)
-            <li class="odd:bg-red-500 odd:hover:bg-red-600">
+            <li class="odd:bg-red-500 odd:hover:bg-red-600 bg-secondary-500 hover:bg-secondary-600 rounded-md">
                 <a target="_blank" href="{{ $data->wiki_url }}/{{ $page['path'] }}"
-                    class="px-2 py-2 flex items-center gap-x-2 rounded-md transition-all ease-in-out">
+                    class="px-2 py-2 flex items-center gap-x-2 transition-all ease-in-out">
                     <i class="fa-solid fa-caret-right me-2"></i>
                     {{ $page['title'] }}
                 </a>
