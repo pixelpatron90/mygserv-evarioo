69bd325c0798dde3e5df74e5e7b630796c1ff9e9
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 11:57:41 +0100

    Update

--- views/layouts/maintenance.blade.php
+++ views/layouts/maintenance.blade.php
@@ -7,20 +7,19 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     @vite(['themes/' . config('settings::theme-active') . '/js/app.js', 'themes/' . config('settings::theme-active') .
     '/css/app.css'], config('settings::theme-active'))
-    <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
     @if (config('settings::app_logo'))
     <link rel="icon" href="{{ asset(config('settings::app_logo')) }}" type="image/png" />
     @else
     <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
     @endif
-    <meta content="{{ ucfirst($title) ?? config('settings::seo_title') }}" property="og:title" />
-    <meta content="{{ $description ?? config('settings::seo_description') }}" property="og:description" />
-    <meta content="{{ $description ?? config('settings::seo_description') }}" name="description" />
-    <meta content='{{ $image ?? config(' settings::seo_image') }}' property='og:image'>
-    <link type="application/json+oembed"
-        href="{{ url('/') }}/manifest.json?title={{ config('app.name') }}&author_url={{ url('/') }}&author_name={{ config('app.name') }}" />
+
+    <meta content="{{ config('settings::seo_title') }}" property="og:title" />
+    <meta content="{{ config('settings::seo_description') }}" property="og:description" />
+    <meta content="{{ config('settings::seo_description') }}" name="description" />
+    <meta content='{{ config(' settings::seo_image') }}' property='og:image'>
     <meta name="twitter:card" content="@if (config('settings::seo_twitter_card')) summary_large_image @endif" />
+
     <title>{{ config('app.name') }} | {{ __('Authentication') }}</title>
 </head>
 
