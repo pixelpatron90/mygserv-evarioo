e6bcf5af7400004fd1393723718d280fd590969c
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 16:38:36 +0100

    Update

--- views/evarioo/wikijs/widget/pagination.blade.php
+++ views/evarioo/wikijs/widget/pagination.blade.php
@@ -10,7 +10,7 @@
             @if (is_string($element))
             <span aria-disabled="true">
                 <span
-                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 cursor-default leading-5 dark:bg-gray-800 dark:border-gray-600">
+                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium last:rounded-s-md text-gray-700 bg-white border border-gray-300 cursor-default leading-5 dark:bg-gray-800 dark:border-gray-600">
                     {{ $element }}
                 </span>
             </span>
@@ -22,7 +22,7 @@
             @if ($page == $paginator->currentPage())
             <span aria-current="page">
                 <span
-                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-gray-800 border border-primarylight cursor-default leading-5">
+                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white last:rounded-e-md bg-gray-800 border border-primarylight cursor-default leading-5">
                     {{ $page }}
                 </span>
             </span>
