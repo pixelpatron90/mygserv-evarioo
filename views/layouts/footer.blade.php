daf7bdb7a1ea2706f63038f23837293a6cd620b8
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 12:18:55 +0100

    Update

--- views/layouts/footer.blade.php
+++ views/layouts/footer.blade.php
@@ -1,8 +1,11 @@
 <footer>
     <div class="container">
-        <p>Copyright © {{ date('Y') }} <a class="text-red-500 hover:text-red-600" href="{{ route('index') }}">{{ config('app.name') }}</a> & <a class="text-red-500 hover:text-red-600" href="https://www.evarioo.eu">www.evarioo.eu</a> | Alle Rechte vorbehalten.</p>
+        <p>Copyright © {{ date('Y') }} <a class="text-red-500 hover:text-red-600" href="{{ route('index') }}">{{
+                config('app.name') }}</a> & <a class="text-red-500 hover:text-red-600"
+                href="https://www.evarioo.eu">www.evarioo.eu</a> | Alle Rechte vorbehalten.</p>
         <ul>
             <li><a href="{{ route('legalnotice.index') }}">{{ __('Legal notice') }}</a></li>
+            <li><a href="{{ route('tos') }}">{{ __('Terms of service') }}</a></li>
             <li><a href="{{ route('media.index') }}">{{ __('Media') }}</a></li>
         </ul>
     </div>
