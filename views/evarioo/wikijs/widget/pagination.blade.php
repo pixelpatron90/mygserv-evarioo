1316721b58ec4eb98d05d5c0ca05c54e680fed37
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 16:39:51 +0100

    Update

--- views/evarioo/wikijs/widget/pagination.blade.php
+++ views/evarioo/wikijs/widget/pagination.blade.php
@@ -22,13 +22,13 @@
             @if ($page == $paginator->currentPage())
             <span aria-current="page">
                 <span
-                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white last:rounded-e-md bg-gray-800 border border-primarylight cursor-default leading-5">
+                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-gray-800 border border-primarylight cursor-default leading-5">
                     {{ $page }}
                 </span>
             </span>
             @else
             <a href="{{ $url }}"
-                class="active:bg-red-500 relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium last:rounded-e-md text-white bg-gray-800 hover:bg-red-500 border border-primarylight leading-5 hover:text-white focus:z-10 focus:outline-none focus:none ring-gray-300 transition ease-in-out duration-150"
+                class="active:bg-red-500 relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-white bg-gray-800 hover:bg-red-500 border border-primarylight leading-5 hover:text-white focus:z-10 focus:outline-none focus:none ring-gray-300 transition ease-in-out duration-150"
                 aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                 {{ $page }}
             </a>
