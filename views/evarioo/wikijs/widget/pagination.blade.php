35fa8b2153a993c43762bf1948c282f926d655c8
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 16:32:14 +0100

    Update

--- views/evarioo/wikijs/widget/pagination.blade.php
+++ views/evarioo/wikijs/widget/pagination.blade.php
@@ -1,4 +1,5 @@
-<nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="mt-4 flex items-center justify-between">
+@if ($paginator->hasPages())
+<div role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="mt-4 flex items-center justify-between">
     <div class="flex justify-between flex-1 sm:hidden">
         @if ($paginator->onFirstPage())
         <span
@@ -129,4 +130,5 @@
             </span>
         </div>
     </div>
-</nav>
+</div>
+@endif
