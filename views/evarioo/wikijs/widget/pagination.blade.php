41903b75794d2409d91112e282f6141396d31c4b
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 16:39:14 +0100

    Update

--- views/evarioo/wikijs/widget/pagination.blade.php
+++ views/evarioo/wikijs/widget/pagination.blade.php
@@ -10,7 +10,7 @@
             @if (is_string($element))
             <span aria-disabled="true">
                 <span
-                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium last:rounded-s-md text-gray-700 bg-white border border-gray-300 cursor-default leading-5 dark:bg-gray-800 dark:border-gray-600">
+                    class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium first:rounded-s-md text-gray-700 bg-white border border-gray-300 cursor-default leading-5 dark:bg-gray-800 dark:border-gray-600">
                     {{ $element }}
                 </span>
             </span>
