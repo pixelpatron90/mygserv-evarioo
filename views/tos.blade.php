ce3f3be9c2a33c4319a2d606151a85de45c029e4
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 12:17:06 +0100

    Update

--- views/tos.blade.php
+++ views/tos.blade.php
@@ -7,9 +7,7 @@
             <p class="mb-4 text-lg font-light text-secondary-200 italic">
                 {{ __('Last Updated:') }} {{ config('settings::tos_last_updated') }}
             </p>
-            <div>
-                @markdownify(config('settings::tos_text'))
-            </div>
+            @markdownify(config('settings::tos_text'))
         </div>
     </div>
 </x-app-layout>
